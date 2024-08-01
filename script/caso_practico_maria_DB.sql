6e81b793921d03575da9c02c4cbd22ac51c8d6f9
Darío Villegas <102450136+dovillegas@users.noreply.github.com>
2024-08-01 11:31:09 -0300

    Update caso_practico_maria_DB.sql

--- script/caso_practico_maria_DB.sql
+++ script/caso_practico_maria_DB.sql
@@ -7,9 +7,8 @@ CREATE TABLE estudiantes (
     perfil_tecnico VARCHAR(100) NOT NULL,
     correo_electronico VARCHAR(255) NOT NULL UNIQUE,
     estado_verificacion_correo TINYINT(1) NOT NULL DEFAULT 0,
-    dni CHAR(8) NOT NULL UNIQUE,
+    dni CHAR(8) NOT NULL UNIQUE
 );
-
 
 CREATE TABLE cursos (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -27,6 +26,8 @@ CREATE TABLE intereses (
 CREATE TABLE inscripciones (
     id_estudiante INT,
     id_curso INT,
+    codigo_inscripcion VARCHAR(64) NOT NULL,
+    fecha_inscripcion DATE NOT NULL,
     PRIMARY KEY (id_estudiante, id_curso),
     FOREIGN KEY (id_estudiante) REFERENCES estudiantes(id),
     FOREIGN KEY (id_curso) REFERENCES cursos(id)
